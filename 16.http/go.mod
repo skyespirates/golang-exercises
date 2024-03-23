fc3e6e08aeee36d9443d29dc799ecd2c685105df
Aziz <akashiseijurou414@gmail.com>
2024-03-23 22:41:31 +0700

    http basic

---
+++ 16.http/go.mod
@@ -0,0 +1,5 @@
+module hatetepe
+
+go 1.22.1
+
+require golang.org/x/net v0.22.0 // indirect
---
+++ 16.http/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
